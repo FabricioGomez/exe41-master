0a323a293b2f2736ff55bd181d77486939fe2557
Gomufas <46228034+Gomufas@users.noreply.github.com>
2021-03-03 19:13:26 -0800

    editing the styling

--- src/App.js
+++ src/App.js
@@ -16,14 +16,21 @@ function App({ data, getData }) {
           <p>no data</p>
         ) : (
           <VictoryChart
+            height={200}
+            width={300}
             theme={VictoryTheme.material}
             domainPadding={20}
             domain={{
               x: [
                 data.data.length > 100 ? data.data.length - 100 : 0,
-                data.data.length+1
-                
+                data.data.length + 1,
               ],
+            }}
+            style={{
+              parent: {
+                width: "80vw"
+              },
+            
             }}
           >
             <VictoryAxis
@@ -31,25 +38,25 @@ function App({ data, getData }) {
               label="Date"
               axisLabelComponent={<VictoryLabel dy={30} />}
               tickLabelComponent={<VictoryLabel angle={30} />}
+              style={{
+                tickLabels: { fontSize: 5 },
+                axisLabel: { fontSize: 5 },
+              }}
             />
             <VictoryAxis
               dependentAxis
               tickFormat={(x) => `${x}`}
               label="Open"
               axisLabelComponent={<VictoryLabel angle={0} dx={-25} />}
+              style={{
+                tickLabels: { fontSize: 5 },
+                axisLabel: { fontSize: 5 },
+              }}
             />
             <VictoryLine data={data.data} y="Open" />
           </VictoryChart>
         )}
       </header>
-      <button
-        className="button"
-        onClick={() => {
-          getData(0);
-        }}
-      >
-        get next
-      </button>
     </div>
   );
 }
